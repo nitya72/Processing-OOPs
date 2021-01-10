885c84603ceca6c5275a63d71542d6779d33e1ec
Nitya Arora <nityaarora72@gmail.com>
2021-01-10 21:08:20 +0530

    Code refactored

--- src/main/java/Ball.java
+++ src/main/java/Ball.java
@@ -1,51 +1,24 @@
+import processing.core.PApplet;
+
 public class Ball {
 
-    private final Processing processing;
-    private float xPos;
-    private float yPos;
+    private float xPosition;
+    private final float yPosition;
     private final int diameter;
-    private int speed;
+    private final int speed;
 
-    public enum BallColors {RED, BLUE, GREEN, PURPLE, YELLOW, WHITE, BLACK}
-
-    public Ball(Processing processing, float xPos, float yPos, int speed, int diameter) {
-        this.processing = processing;
-        this.xPos = xPos;
-        this.yPos = yPos;
+    public Ball(float xPosition, float yPosition, int speed, int diameter) {
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
         this.speed = speed;
         this.diameter = diameter;
     }
 
-    public void colorBall(BallColors ballColors) {
-        switch (ballColors) {
-            case RED:
-                processing.fill(255, 0, 0);
-                break;
-            case BLUE:
-                processing.fill(0, 0, 255);
-                break;
-            case GREEN:
-                processing.fill(0, 128, 0);
-                break;
-            case PURPLE:
-                processing.fill(128, 0, 128);
-                break;
-            case YELLOW:
-                processing.fill(255, 255, 0);
-                break;
-            case WHITE:
-                processing.fill(0, 0, 0);
-                break;
-            case BLACK:
-                processing.fill(255, 255, 255);
-                break;
-            default:
-                System.out.println("Color not found");
-        }
+    public void drawCircle(PApplet pApplet) {
+        pApplet.ellipse(xPosition, yPosition, diameter, diameter);
     }
 
-    public void drawBall() {
-        processing.ellipse(xPos, yPos, diameter, diameter);
-        xPos += speed;
+    public void move() {
+        xPosition += speed;
     }
 }
--- src/main/java/Processing.java
+++ src/main/java/Processing.java
@@ -1,15 +1,20 @@
 import processing.core.PApplet;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Processing extends PApplet {
 
     public static final int WIDTH = 640;
     public static final int HEIGHT = 480;
     public static final int DIAMETER = 25;
 
-    private Ball ball1;
-    private Ball ball2;
-    private Ball ball3;
-    private Ball ball4;
+    public static final int SPEED_BALL1 = 1;
+    public static final int SPEED_BALL2 = 2;
+    public static final int SPEED_BALL3 = 3;
+    public static final int SPEED_BALL4 = 4;
+
+    private List<Ball> ballList;
 
     public static void main(String[] args) {
         PApplet.main("Processing", args);
@@ -22,24 +27,18 @@ public class Processing extends PApplet {
 
     @Override
     public void setup() {
-        ball1 = new Ball(this, 0, HEIGHT * 0.2f, 1, DIAMETER);
-        ball2 = new Ball(this, 0, HEIGHT * 0.4f, 2, DIAMETER);
-        ball3 = new Ball(this, 0, HEIGHT * 0.6f, 3, DIAMETER);
-        ball4 = new Ball(this, 0, HEIGHT * 0.8f, 4, DIAMETER);
+        Ball ball1 = new Ball(0, HEIGHT * 0.2f, SPEED_BALL1, DIAMETER);
+        Ball ball2 = new Ball(0, HEIGHT * 0.4f, SPEED_BALL2, DIAMETER);
+        Ball ball3 = new Ball(0, HEIGHT * 0.6f, SPEED_BALL3, DIAMETER);
+        Ball ball4 = new Ball(0, HEIGHT * 0.8f, SPEED_BALL4, DIAMETER);
+
+        ballList = Arrays.asList(ball1, ball2, ball3, ball4);
     }
 
     @Override
     public void draw() {
-        ball1.colorBall(Ball.BallColors.RED);
-        ball1.drawBall();
+        ballList.forEach(Ball::move);
 
-        ball2.colorBall(Ball.BallColors.YELLOW);
-        ball2.drawBall();
-
-        ball3.colorBall(Ball.BallColors.GREEN);
-        ball3.drawBall();
-
-        ball4.colorBall(Ball.BallColors.PURPLE);
-        ball4.drawBall();
+        ballList.forEach(ball -> ball.drawCircle(this));
     }
 }
